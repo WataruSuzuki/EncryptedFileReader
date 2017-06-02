0848624b8ca0c3c2f7a86513faf2115762ffc882
WataruSuzuki <wataru0406@gmail.com>
2017-06-02 22:10:00 +0900

    Added function that encryption & decryption.

--- app/src/main/java/com/devjchankchan/encryptedfilereader/MainActivity.java
+++ app/src/main/java/com/devjchankchan/encryptedfilereader/MainActivity.java
@@ -1,14 +1,29 @@
 package com.devjchankchan.encryptedfilereader;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String AES = "AES";
+    private static final String KEY = "0123456789ABCDEF";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +31,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView textView = (TextView) findViewById(R.id.textview);
-        textView.setText(loadText());
+        try {
+            textView.setText(decrypt(loadText(), KEY));
+        } catch (Exception e) {
+
+        }
+//        writeFile();
     }
 
+    private void writeFile() {
+        try {
+            FileWriter writer = new FileWriter(new File(Environment.getExternalStorageDirectory().toString(), "test.txt"));
+            writer.write(encrypt(loadText(), KEY));
+            writer.close();
+        } catch (Exception e) {
+
+        }
+    }
 
     private String loadText() {
         BufferedReader reader = null;
@@ -34,5 +63,37 @@ public class MainActivity extends AppCompatActivity {
         } catch (IOException e) {
             return "(・A・)";
         }
+    }
+
+    private String encrypt(String originalString, String secretKey) throws NoSuchAlgorithmException,
+            NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+
+        byte[] originalBytes = originalString.getBytes();
+        byte[] secretKeyBytes = secretKey.getBytes();
+
+        SecretKeySpec secretKeySpec = new SecretKeySpec(secretKeyBytes, AES);
+        Cipher cipher = Cipher.getInstance(AES);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
+        byte[] encryptBytes = cipher.doFinal(originalBytes);
+        byte[] encryptBytesBase64 = Base64.encode(encryptBytes, Base64.DEFAULT);
+//        byte[] encryptBytesBase64 = Base64.encodeBase64(encryptBytes, false);
+
+        return new String(encryptBytesBase64);
+    }
+
+    private String decrypt(String encryptBytesBase64String, String secretKey)
+            throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException,
+            IllegalBlockSizeException, BadPaddingException {
+
+        byte[] encryptBytes = Base64.decode(encryptBytesBase64String, Base64.DEFAULT);
+//        byte[] encryptBytes = Base64.decodeBase64(encryptBytesBase64String);
+        byte[] secretKeyBytes = secretKey.getBytes();
+
+        SecretKeySpec secretKeySpec = new SecretKeySpec(secretKeyBytes, AES);
+        Cipher cipher = Cipher.getInstance(AES);
+        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
+        byte[] originalBytes = cipher.doFinal(encryptBytes);
+
+        return new String(originalBytes);
     }
 }
